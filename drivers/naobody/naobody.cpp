b8fba6b1a5fb246f07c631aa4b1d59cfa01c66b6
eperdes <eperdes@2fa0627c-b022-0410-acda-9783b4e515f1>
2010-03-09 10:33:29 +0000

    Actualizado progeo, corregido bug de displayline

--- drivers/naobody/naobody.cpp
+++ drivers/naobody/naobody.cpp
@@ -1,6 +1,5 @@
 /*
- *
- *  Copyright (C) 1997-2008 JDE Developers Team
+ *  Copyright (C) 1997-2010 JDE Developers Team
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -15,9 +14,8 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see http://www.gnu.org/licenses/.
  *
- *  Authors : 	Eduardo Perdices <edupergar@gmail.com>
+ *  Authors : 	Eduardo Perdices <eperdes@gsyc.es>
  *				Francisco Rivas <fm.rivas@alumnos.urjc.es>
- *
  */
 
 #include <cstdlib>
