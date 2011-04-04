cd81493c9167593d24dc24d8951017c27e22f22a
Alejandro Hernández Cordero <ahcorde@gmail.com>
2011-04-04 07:21:59 +0000

    Subo las versiones oficiales de colortuner y opencvdemo

--- src/components/opencvdemo/opencvdemo.cpp
+++ src/components/opencvdemo/opencvdemo.cpp
@@ -1,55 +1,88 @@
-#include <iostream>
-#include <string>
-#include <gtkmm.h>
-#include <libglademm.h>
-#include <IceUtil/Thread.h>
-#include <IceUtil/Time.h>
-#include <gbxsickacfr/gbxiceutilacfr/store.h>
-#include <jderobotice/component.h>
-#include <jderobotice/application.h>
-#include <jderobotutil/observer.h>
-#include <jderobot/varcolor.h>
-#include "view.h"
-#include "model.h"
+/*
+ *
+ *  Copyright (C) 1997-2009 JDERobot Developers Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/. 
+ *
+ *  Authors : Rubén González Barriada <ruben.gbarriada@gmail.com>
+ *						Alejandro Hernández Cordero <ahcorde@gmail.com> 
+ *
+ */
 
-namespace opencvdemo {
-  class Component: public jderobotice::Component{
-  public:
-    Component()
-      : jderobotice::Component("Opencvdemo"),model(0),controller(0) {
-      model = new Model();
-      controller = new Controller(model);
+#include <Ice/Ice.h>
+#include <jderobot/camera.h>
+#include <colorspaces/colorspacesmm.h>
+#include "viewer.h"
+
+int main(int argc, char** argv){
+  int status;
+  opencvdemo::Viewer viewer;
+  Ice::CommunicatorPtr ic;
+
+  try{
+    ic = Ice::initialize(argc,argv);
+    Ice::ObjectPrx base = ic->propertyToProxy("Opencvdemo.Camera.Proxy");
+    if (0==base)
+      throw "Could not create proxy";
+
+    /*cast to CameraPrx*/
+    jderobot::CameraPrx cprx = jderobot::CameraPrx::checkedCast(base);
+    if (0==cprx)
+      throw "Invalid proxy";
+
+    while(viewer.isVisible()){
+      
+			jderobot::ImageDataPtr data = cprx->getImageData();
+      colorspaces::Image::FormatPtr fmt = colorspaces::Image::Format::searchFormat(data->description->format);
+
+      if (!fmt)
+				throw "Format not supported";
+
+			char * data1 = new char[data->description->width*data->description->height*3];
+
+			memcpy(data1, &(data->pixelData[0]), data->description->width*data->description->height*3);
+
+			// Get input image 
+      colorspaces::Image image(data->description->width,
+			       data->description->height,
+			       fmt,
+			       &(data->pixelData[0]));
+	
+			// Get output image
+     colorspaces::Image image2(data->description->width,
+			       data->description->height,
+			       fmt,
+			       data1);
+
+			// Selecting the operation
+			viewer.selection(image2);
+      
+			// Displaying the images
+			viewer.display(image,image2);
+
+			delete data1;
+	
     }
+  }catch (const Ice::Exception& ex) {
+    std::cerr << ex << std::endl;
+    status = 1;
+  } catch (const char* msg) {
+    std::cerr << msg << std::endl;
+    status = 1;
+  }
 
-    virtual void start(){
-      /*inicializamos proxy a pelo*/
-      Ice::ObjectPrx base = context().communicator()->stringToProxy("varcolorA:tcp -h 127.0.0.1 -p 9999");
-      /*lo correcto es hacerlo con propiedades para poder modificar 
-	el proxy sin tener que recompilar*/
-      
-      /*Ice::ObjectPrx base = ic->propertyToProxy("VarColorView.VarColor.Proxy");*/
-      if (0==base)
-	throw "Could not create proxy";
-      
-      /*cast to VarColorPrx*/
-      jderobot::VarColorPrx vprx = jderobot::VarColorPrx::checkedCast(base);
-      if (0==vprx)
-	throw "Invalid proxy";
-      
-      while(controller.isRunning()){
-	/*remote procedure call*/
-	model.setImage(vprx->getData());
-      }
-    }
-  private:
-    Model* model;
-    Controller* controller;
-  };
-} //namespace
-
-int main(int argc, char * argv[]){
-  opencvdemo::Component component;
-
-  jderobotice::Application app( component );
-  return app.jderobotMain(argc, argv);
+  if (ic)
+    ic->destroy();
+  return status;
 }
