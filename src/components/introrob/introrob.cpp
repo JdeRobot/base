2e4d5620c62be1cd551de89a4312184d88366673
mikel <mikel@2fa0627c-b022-0410-acda-9783b4e515f1>
2012-03-05 18:56:48 +0000

    [Mikel] Moved configuration files to their respectives directories.

--- src/components/introrob/introrob.cpp
+++ src/components/introrob/introrob.cpp
@@ -1,25 +1,3 @@
-/*
- *  Copyright (C) 1997-2011 JDERobot Developers Team
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- *  Authors : Maikel González <m.gonzalezbai@gmail.com>,
- *            Jose María Cañas Plaza <jmplaza@gsyc.es>
- *
- */
-
 #include "control.h"
 #include "API.h"
 #include "gui.h"
@@ -101,9 +79,9 @@ int main(int argc, char** argv){
    }
 
    api = new introrob::Api();
-   api->showImage=false;
+   //api->showImage=true;
    if((argc==3)&&(!strcmp(argv[2],"-d"))){
-      cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE);
+      
       api->showImage=true;
    }
 
