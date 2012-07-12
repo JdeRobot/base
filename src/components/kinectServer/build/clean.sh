4adf5273f82dc870b8b16bdf719eee5e68c14e1e
mikel <mikel@2fa0627c-b022-0410-acda-9783b4e515f1>
2012-07-12 21:40:53 +0000

    Mejorando CMake: Añadidas opciones de instalar y desinstalar (sudo make install) por cada componente con sus librerias.

--- src/components/kinectServer/build/clean.sh
+++ src/components/kinectServer/build/clean.sh
@@ -7,9 +7,11 @@ rm -r ../CMakeFiles
 mkdir ../temp
 mv CMakeLists.txt ../temp
 mv clean.sh ../temp
+mv cmake_uninstall.cmake.in ../temp
 rm -r *
 mv ../temp/CMakeLists.txt .
 mv ../temp/clean.sh .
+mv ../temp/cmake_uninstall.cmake.in .
 rm -r ../temp
 cd ../../../../
 ./clean_repository 
