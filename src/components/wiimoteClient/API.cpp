12ad9528d8c15f7461952d974a08d0352cdc4eb7
mikel <mikel@2fa0627c-b022-0410-acda-9783b4e515f1>
2012-09-13 21:55:52 +0000

    [Mikel] Finalizado driver Wiimote: 	+ Mejorada la GUI arreglando algunos bugs en el stick del nunchuk.

--- src/components/wiimoteClient/API.cpp
+++ src/components/wiimoteClient/API.cpp
@@ -1,21 +1,23 @@
+// <editor-fold defaultstate="collapsed" desc="comment">
 /*
- *  Copyright (C) 1997-2011 JDERobot Developers Team
+ *  Copyright (C) 1997-2010 JDE Developers Team
  *
- *  This program is free software; you can redistribute it and/or modify
+ *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundationfile:///home/mikel/Escritorio/PFC/repository_JDErobot/Workspace/trunk/src/components/wiimoteClient/wiimoteClient.cpp
+, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
+ *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *g
- *  Authors : Maikel González <m.gonzalezbai@gmail.com>,
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ *  Authors : Jose María Cañas <jmplaza@gsyc.es>
+ *            Maikel González Baile <m.gonzalezbai@gmail.com>
  *
  */
 
