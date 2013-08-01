2f1dc506942f78a9b8c19683e90b6b003afc6d90
rocapal <rocapal@gmail.com>
2013-08-01 10:39:51 +0000

    [26] Added new constructors ans setters. Header define changed

--- src/libs/geometry/progeo/progeo2.h
+++ src/libs/geometry/progeo/progeo2.h
@@ -1,8 +1,30 @@
-#ifndef PROGEO_H
-#define PROGEO_H
+/*
+ *
+ *  Copyright (C) 1997-2013 JDERobot Developers Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/. 
+ *
+ *  Authors : Alejandro Hernández <ahcorde [at] gmail [dot] com>
+ *            Roberto Calvo <rocapal [at] gsyc [dot] urjc [dot] es>
+ *
+ */
+
+#ifndef PROGEO2_H
+#define PROGEO2_H
 
 #define EIGEN_DONT_ALIGN_STATICALLY True
- 
+
 // standard C
 #include <iostream>
 
@@ -31,62 +53,76 @@
 
 namespace Progeo {
 
-    class Progeo
-    {
+class Progeo
+{
 
-        public:Progeo();
+public:
 
-        public: void xmlReader(std::string filename);
+    Progeo();
+    Progeo(std::string filename);
 
-        public: void display_camerainfo();
+    Progeo(math::Vector3H posCamera,
+           math::Matriz3x3 KMatrix,
+           math::Matriz4x4 RTMatrix,
+           int width, int height);
 
-        public: void backproject(math::Vector2H, math::Vector3H& pro);
-        public: int project(math::Vector3H in, math::Vector2H &out);
-        public: void update_camera_matrix();
+    void setPosition (math::Vector3H pos);
+    void setKMatrix (math::Matriz3x3 KMatrix);
+    void setRTMatrix (math::Matriz4x4 RTMatrix);
+    void setImageSize (int width, int height);
+    
+    void display_camerainfo();
+    void backproject(math::Vector2H, math::Vector3H& pro);
+    int project(math::Vector3H in, math::Vector2H &out);
+    void update_camera_matrix();
 
-        /* camera 3d position in mm */
-        private:    math::Vector3H position;
 
-        /* camera 3d focus of attention in mm */
-        private:    math::Vector3H foa;
 
-        /* top right and bottom left points */
-        private:    math::Vector3 tr, bl;
+private:
 
-        /* camera roll position angle in rads */
-        private:    float roll;
+    /* camera 3d position in mm */
+    math::Vector3H position;
 
-        /* focus x distance in mm*/
-        private:    float fdistx;
-        private:    float fdisty;
+    /* camera 3d focus of attention in mm */
+    math::Vector3H foa;
 
-        /* pixels */
-        private:    float u0,v0;
+    /* top right and bottom left points */
+    math::Vector3 tr, bl;
 
-        /*angle between the x and y pixel axes in rads*/
-        private:    float skew
+    /* camera roll position angle in rads */
+    float roll;
 
-        /* image height in pixels */;
-        private:    int rows;
+    /* focus x distance in mm*/
+    float fdistx;
+    float fdisty;
 
-        /* image width in pixels */
-        private:    int columns;
+    /* pixels */
+    float u0,v0;
 
-        /* camera K matrix */
-        private:    float k11, k12, k13, k14, k21, k22, k23, k24, k31, k32, k33, k34;
-        private:    math::Matriz3x3 K;
+    /*angle between the x and y pixel axes in rads*/
+    float skew
 
-        /* camera rotation + translation matrix */
-        private:    float rt11, rt12, rt13, rt14, rt21, rt22, rt23, rt24, rt31, rt32, rt33, rt34, rt41, rt42, rt43, rt44;
-        private:    math::Matriz4x4 RT;
+    /* image height in pixels */;
+    int rows;
 
-          /* distortion parameters */
-        private:     float d1,d2,d3,d4,d5,d6;
-        private:     float dx,dy;
+    /* image width in pixels */
+    int columns;
 
-        /* name */
-        std::string name;
-    };
+    /* camera K matrix */
+    float k11, k12, k13, k14, k21, k22, k23, k24, k31, k32, k33, k34;
+    math::Matriz3x3 K;
+
+    /* camera rotation + translation matrix */
+    float rt11, rt12, rt13, rt14, rt21, rt22, rt23, rt24, rt31, rt32, rt33, rt34, rt41, rt42, rt43, rt44;
+    math::Matriz4x4 RT;
+
+    /* distortion parameters */
+    float d1,d2,d3,d4,d5,d6;
+    float dx,dy;
+
+    /* name */
+    std::string name;
+};
 }
 
-#endif 
+#endif
