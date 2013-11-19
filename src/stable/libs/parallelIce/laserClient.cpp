28cf8fb16168911ccaf6619d2fef8b51e71eebdd
Francisco Rivas <gchanfr@gmail.com>
2013-11-19 21:50:13 +0000

    #110 included aux functions to parallelICe

--- src/stable/libs/parallelIce/laserClient.cpp
+++ src/stable/libs/parallelIce/laserClient.cpp
@@ -1,10 +1,23 @@
 /*
- * laserClient.cpp
+ *  Copyright (C) 1997-2013 JDE Developers TeamkinectViewer.camRGB
  *
- *  Created on: 23/07/2013
- *      Author: frivas
- */
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ *  Author : Jose María Cañas <jmplaza@gsyc.es>
+			Francisco Miguel Rivas Montero <franciscomiguel.rivas@urjc.es>
 
+ */
 #include "laserClient.h"
 
 namespace jderobot {
