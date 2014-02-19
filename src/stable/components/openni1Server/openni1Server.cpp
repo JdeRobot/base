2c78795ff66cf2fbf34dbdb7e7ba19f68776c0d9
Francisco Rivas <gchanfr@gmail.com>
2014-02-19 17:10:18 +0000

    #175 openni1Server set as default to 640x480@30 (other configurations do not work)

--- src/stable/components/openni1Server/openni1Server.cpp
+++ src/stable/components/openni1Server/openni1Server.cpp
@@ -30,7 +30,7 @@
 #include <jderobot/pose3dmotors.h>
 #include <jderobot/remoteCloud.h>
 #include <jderobot/remoteConfig.h>
-#include <visionlib/colorspaces/colorspacesmm.h>
+#include <colorspaces/colorspacesmm.h>
 #include <tr1/memory>
 #include <list>
 #include <sstream>
@@ -84,7 +84,7 @@ xn::Context g_context;
 std::vector<int> pixelsID;
 //int pixelsID[640*480];
 int userGeneratorActive=0;
-int localDebug;
+int debug;
 
 
 struct KinectDevice
@@ -553,7 +553,7 @@ private:
 			pthread_mutex_unlock(&mutex);
 			if (totalpre !=0){
 				if ((totala - totalpre) > cycle ){
-					if (localDebug)
+					if (debug)
 						std::cout<<"-------- openni1Server: WARNING- RGB timeout-" << std::endl; 
 				}
 				else{
@@ -781,7 +781,7 @@ private:
 			pthread_mutex_unlock(&mutex);
 			if (totalpre !=0){
 				if ((totala - totalpre) > cycle ){
-					if (localDebug)
+					if (debug)
 						std::cout<<"-------- openni1Server: WARNING- DEPTH timeout-" << std::endl; 
 				}
 				else{
@@ -985,7 +985,7 @@ private:
 					pthread_mutex_unlock(&mutex);
 					if (totalpre !=0){
 						if ((totala - totalpre) > cycle ){
-							if (localDebug)
+							if (debug)
 								std::cout<<"-------- openni1Server: WARNING- POINTCLOUD timeout-" << std::endl; 
 						}
 						else{
@@ -1262,7 +1262,7 @@ int main(int argc, char** argv){
 	int pointCloud = prop->getPropertyAsIntWithDefault(componentPrefix + ".pointCloudActive",0);
 	int playerdetection = prop->getPropertyAsIntWithDefault(componentPrefix + ".PlayerDetection",0);
 	width=prop->getPropertyAsIntWithDefault("openni1Server.Width", 640);
-	localDebug=prop->getPropertyAsIntWithDefault("openni1Server.Debug", 0);
+	debug=prop->getPropertyAsIntWithDefault("openni1Server.Debug", 0);
 	height=prop->getPropertyAsIntWithDefault("openni1Server.Height",480);
 	int fps=prop->getPropertyAsIntWithDefault("openni1Server.Fps",30);
 
@@ -1469,7 +1469,7 @@ int main(int argc, char** argv){
 				}
 				delete[] aModeD;
 
-				sensors[i].depth.SetMapOutputMode(aModeR[4]);
+				sensors[i].depth.SetMapOutputMode(aModeR[2]);
 
 				#ifdef WITH_NITE
 				rc = g_context.CreateAnyProductionTree(XN_NODE_TYPE_USER, &query, sensors[i].g_UserGenerator);
@@ -1480,7 +1480,7 @@ int main(int argc, char** argv){
 					rgb_mode.nXRes = width;
 					rgb_mode.nYRes = height;
 					rgb_mode.nFPS = fps;
-					sensors[i].image.SetMapOutputMode(aModeD[4]);
+					sensors[i].image.SetMapOutputMode(aModeD[2]);
 
 
 				sensors[i].depth.GetAlternativeViewPointCap().SetViewPoint(sensors[i].image);
