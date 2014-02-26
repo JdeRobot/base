9fde99c0731c8f21378cc093ea9cda9e5ef760ba
jmplaza <jmplaza@users.noreply.github.com>
2014-02-26 11:23:01 +0000

    [jmplaza] #57, nuevo contenido gazebo-setup.sh para que funcione con ubuntu 12.04 tanto en 32bits como 64bits, y debian 7.

--- src/stable/components/gazeboserver/gazebo-setup.sh
+++ src/stable/components/gazeboserver/gazebo-setup.sh
@@ -3,5 +3,4 @@ export GAZEBO_MODEL_DATABASE_URI=http://gazebosim.org/models
 export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-1.8:/usr/share/gazebo_models:${HOME}/.gazebo/models
 export GAZEBO_PLUGIN_PATH=/usr/lib/gazebo-1.8/plugins:/usr/lib/gazebo-1.8/plugins-jderobot/pioneer:/usr/lib/gazebo-1.8/plugins-jderobot/nao:/usr/lib/gazebo-1.8/plugins-jderobot/kinect
 export LD_LIBRARY_PATH=/usr/lib/gazebo-1.8/plugins:/usr/lib/gazebo-1.8/plugins-jderobot/pioneer:/usr/lib/gazebo-1.8/plugins-jderobot/nao:/usr/lib/gazebo-1.8/plugins-jderobot/kinect:${LD_LIBRARY_PATH}
-export OGRE_RESOURCE_PATH=/usr/lib/$(uname -i)-linux-gnu/OGRE-1.7.4
-
+export OGRE_RESOURCE_PATH=/usr/lib/i386-linux-gnu/OGRE-1.7.4:/usr/lib/x86_64-linux-gnu/OGRE-1.7.4:"/usr/lib/*-linux-gnu/OGRE-*":${OGRE_RESOURCE_PATH}
