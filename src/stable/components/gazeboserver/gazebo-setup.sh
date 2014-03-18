d0f0637f868c64e5f4a91d984c1461886e0d367a
rocapal <rocapal@gmail.com>
2014-03-18 16:13:47 +0000

    #197 update script with new paths of plugins

--- src/stable/components/gazeboserver/gazebo-setup.sh
+++ src/stable/components/gazeboserver/gazebo-setup.sh
@@ -1,6 +1,8 @@
 export GAZEBO_MASTER_URI=http://localhost:11345
 export GAZEBO_MODEL_DATABASE_URI=http://gazebosim.org/models
 export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-1.8:/usr/share/gazebo_models:${HOME}/.gazebo/models
-export GAZEBO_PLUGIN_PATH=/usr/lib/gazebo-1.8/plugins:/usr/lib/gazebo-1.8/plugins-jderobot/pioneer:/usr/lib/gazebo-1.8/plugins-jderobot/nao:/usr/lib/gazebo-1.8/plugins-jderobot/kinect
-export LD_LIBRARY_PATH=/usr/lib/gazebo-1.8/plugins:/usr/lib/gazebo-1.8/plugins-jderobot/pioneer:/usr/lib/gazebo-1.8/plugins-jderobot/nao:/usr/lib/gazebo-1.8/plugins-jderobot/kinect:${LD_LIBRARY_PATH}
+export GAZEBO_PLUGIN_PATH=/usr/lib/gazebo-1.8/plugins:/usr/local/share/jderobot/gazebo/plugins/pioneer:/usr/local/share/jderobot/gazebo/plugins/nao:/usr/local/share/jderobot/gazebo/plugins/kinect
+export LD_LIBRARY_PATH=/usr/lib/gazebo-1.8/plugins:/usr/local/share/jderobot/gazebo/plugins/pioneer:/usr/local/share/jderobot/gazebo/plugins/nao:/usr/local/share/jderobot/gazebo/plugins/kinect:${LD_LIBRARY_PATH}
 export OGRE_RESOURCE_PATH=/usr/lib/i386-linux-gnu/OGRE-1.7.4:/usr/lib/x86_64-linux-gnu/OGRE-1.7.4:"/usr/lib/*-linux-gnu/OGRE-*":${OGRE_RESOURCE_PATH}
+
+
