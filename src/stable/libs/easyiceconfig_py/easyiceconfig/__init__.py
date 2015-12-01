c1d7187b93151964dbc57f75776739af0988af50
Victor Arribas <v.arribas.urjc@gmail.com>
2015-12-01 21:13:55 +0100

    easyiceconfig_py: simplifies import sentence

--- src/stable/libs/easyiceconfig_py/easyiceconfig/__init__.py
+++ src/stable/libs/easyiceconfig_py/easyiceconfig/__init__.py
@@ -1,1 +1,9 @@
 __author__ = 'varribas'
+
+## import simplification
+# empty __init__.py:
+#   from easyiceconfig import easyiceconfig as EasyIce
+# new option:
+#   import easyiceconfig as EasyIce
+from .easyiceconfig import *
+
