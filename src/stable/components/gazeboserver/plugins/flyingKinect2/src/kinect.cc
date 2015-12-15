10d18c9a3f712f8d6a39b7a6d960ea0476b2efc0
Victor Arribas <v.arribas.urjc@gmail.com>
2015-12-15 19:55:32 +0100

    flyingKinect from scratch (2/2): CameraRGB+Depth
    * Using single event mode triggered by parent sensor
    * Using quadrotor2 cameras (sprobe of concept SUCCESS)

--- src/stable/components/gazeboserver/plugins/flyingKinect2/src/kinect.cc
+++ src/stable/components/gazeboserver/plugins/flyingKinect2/src/kinect.cc
@@ -17,14 +17,21 @@
  *       Victor Arribas Raigadas <v.arribas.urjc@gmai.com>
  */
 
+// OpenCV
+#include <opencv2/core/core.hpp>
+
 // Gazebo
 #include <gazebo/gazebo.hh>
 #include <gazebo/common/common.hh>
-#include <gazebo/physics/Model.hh>
-#include <gazebo/sensors/sensors.hh>
-#include <gazebo/sensors/SensorManager.hh>
 #include <gazebo/common/Events.hh>
+
+#include <gazebo/physics/Model.hh>
+#include <gazebo/physics/Link.hh>
 #include <gazebo/math/Pose.hh>
+
+#include <sensors/DepthCameraSensor.hh>
+#include <rendering/DepthCamera.hh>
+#include <gazebo/sensors/SensorManager.hh>
 
 // Ice
 #include <Ice/Ice.h>
@@ -34,10 +41,30 @@
 
 // Jderobot
 #include <jderobot/pose3d.h>
+#include <quadrotor/interfaces/pushcamerai.h>
 
+// debug
+#include <quadrotor/debugtools.h>
 
 
 namespace kinect{
+
+void depth2rgb(const cv::Mat &image_raw, cv::Mat &image_8UC3)
+{
+	if (image_raw.size() != image_8UC3.size())
+		image_8UC3.create(image_raw.rows, image_raw.cols, CV_8UC3);
+
+	float *data = (float*)image_raw.data;
+	for ( int i=0; i<image_raw.rows* image_raw.cols; i++ )
+	{
+		int val = (int)(data[i]*1000);
+		image_8UC3.data[3*i+0] = (float)val/10000.*255.;
+		image_8UC3.data[3*i+1] = val>>8;
+		image_8UC3.data[3*i+2] = val&0xff;
+	}
+}
+
+#define PARENT_SENSOR_GETS_UPDATES
 
 class KinectPlugin :
         public gazebo::ModelPlugin,
@@ -48,7 +75,9 @@ public:
         ice_pose3ddata = new jderobot::Pose3DData(0,0,0,0,0,0,0,0);
     }
 
-    ~KinectPlugin(){}
+    virtual ~KinectPlugin(){
+        ONDEBUG_INFO(std::cout << _log_prefix << "QuadrotorPlugin::~QuadrotorPlugin()" << std::endl;)
+    }
 
     std::string _log_prefix;
 
@@ -61,6 +90,7 @@ protected:
         _log_prefix = "["+_model->GetName()+"] ";
         model = _model;
 
+        LoadSensors(_model);
         InitializeIce(_sdf);
 
         sigintConnection = gazebo::event::Events::ConnectSigInt(
@@ -68,13 +98,35 @@ protected:
     }
 
     void Init(){
-        updateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(
-                    boost::bind(&KinectPlugin::OnUpdate, this, _1));
+        ONDEBUG_INFO(std::cout << _log_prefix << "KinectPlugin::Init()" << std::endl;)
+//        updateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(
+//                    boost::bind(&KinectPlugin::OnUpdate, this, _1));
+
+#ifdef PARENT_SENSOR_GETS_UPDATES
+        if (camera_sensor){
+            camera_sensor->SetActive(true);
+
+            sub_camera = camera_sensor->ConnectUpdated(
+                    boost::bind(&KinectPlugin::_on_cam_bootstrap, this));
+        }else{
+            std::cerr << _log_prefix << "\t camera was not connected (NULL pointer)" << std::endl;
+        }
+#else
+        if(camera_sensor && camera_impl){
+            camera_sensor->SetActive(true);
+
+            sub_cam_depth = camera_impl->ConnectNewDepthFrame(
+                    boost::bind(&KinectPlugin::_on_cam_update_depth_data, this, _1,_2,_3,_4,_5));
+            sub_cam_rgb = camera_impl->ConnectNewImageFrame(
+                    boost::bind(&KinectPlugin::_on_cam_update_rgb_data, this, _1,_2,_3,_4,_5));
+        }else{
+            std::cerr << _log_prefix << "\t camera was not connected (NULL pointer)" << std::endl;
+        }
+#endif
     }
 
     void OnUpdate(const gazebo::common::UpdateInfo & /*_info*/){
-        gazebo::math::Pose pose = model->GetWorldPose();
-        ice_pose3ddata = new jderobot::Pose3DData(pose.pos.x, pose.pos.y, pose.pos.z, 1, pose.rot.w, pose.rot.x, pose.rot.y, pose.rot.z);
+        _on_pose_update();
     }
 
     void Reset(){}
@@ -86,6 +138,136 @@ protected:
             std::cout<<_log_prefix << "Ice is down now" << std::endl;
         }
     }
+
+    void LoadSensors(gazebo::physics::ModelPtr _model){
+        ONDEBUG_INFO(std::cout << _log_prefix << "KinectPlugin::LoadSensors()" << std::endl;)
+        gazebo::sensors::SensorManager *sm = gazebo::sensors::SensorManager::Instance();
+        unsigned int base_link_id = _model->GetChild(0)->GetId();
+
+        for (gazebo::sensors::SensorPtr s: sm->GetSensors()){
+            if (s->GetParentId() == base_link_id){
+                camera_sensor = boost::static_pointer_cast<gazebo::sensors::DepthCameraSensor>(s);
+                camera_impl = camera_sensor->GetDepthCamera();
+            }
+        }
+    }
+
+private:
+    gazebo::physics::ModelPtr model;
+    gazebo::event::ConnectionPtr sigintConnection;
+
+/// Gazebo (pose)
+    void _on_pose_update(){
+        gazebo::math::Pose pose = model->GetWorldPose();
+        ice_pose3ddata = new jderobot::Pose3DData(pose.pos.x, pose.pos.y, pose.pos.z, 1, pose.rot.w, pose.rot.x, pose.rot.y, pose.rot.z);
+    }
+
+/// Gazebo (cameras)
+#ifdef PARENT_SENSOR_GETS_UPDATES
+private:
+    void _on_cam_bootstrap(){
+        camera_sensor->DisconnectUpdated(sub_camera);
+
+        if (img_depth.empty()){
+            std::cout <<  _log_prefix << "\tbootstrap depth camera" << std::endl;
+
+            int _width = camera_impl->GetImageWidth();
+            int _height = camera_impl->GetImageHeight();
+
+            const float* depth_data = camera_impl->GetDepthData();
+            img_depth_raw = cv::Mat(_height, _width, CV_32FC1, (float_t*) depth_data);
+            img_depth.create(_height, _width, CV_8UC3);
+            depth2rgb(img_depth_raw, img_depth);
+
+            const unsigned char* rgb_data = camera_impl->GetImageData();
+            img_rgb = cv::Mat(_height, _width, CV_8UC3, (uint8_t*) rgb_data);
+
+            cam_depthI.onCameraSensorBoostrap(img_depth, nullptr);
+            cam_rgbI.onCameraSensorBoostrap(img_rgb, nullptr);
+            _on_pose_update();
+        }
+
+        sub_camera = camera_sensor->ConnectUpdated(
+                    boost::bind(&KinectPlugin::_on_cam_update, this));
+    }
+
+    void _on_cam_update(){
+        // convert image
+        depth2rgb(img_depth_raw, img_depth);
+
+        // stash pose
+        _on_pose_update();
+
+        // push data into interfaces
+        cam_depthI.onCameraSensorUpdate(img_depth);
+        cam_rgbI.onCameraSensorUpdate(img_rgb);
+    }
+#else
+    void _on_cam_bootstrap_depth_data(const float *_data, unsigned int _width, unsigned int _height,
+                                      unsigned int, const std::string &){
+        camera_impl->DisconnectNewDepthFrame(sub_cam_depth);
+
+        img_depth_raw = cv::Mat(_height, _width, CV_32FC1, (float_t*) _data);
+        img_depth.create(_height, _width, CV_8UC3);
+
+        depth2rgb(img_depth_raw, img_depth);
+        cam_depthI.onCameraSensorBoostrap(img_depth, nullptr);
+
+        _on_pose_update();
+
+        sub_cam_depth = camera_impl->ConnectNewDepthFrame(
+                    boost::bind(&KinectPlugin::_on_cam_update_depth_data, this, _1,_2,_3,_4,_5));
+    }
+
+    void _on_cam_update_depth_data(const float *, unsigned int, unsigned int,
+                                   unsigned int, const std::string &){
+        depth2rgb(img_depth_raw, img_depth);
+        cam_depthI.onCameraSensorUpdate(img_depth);
+
+        _on_pose_update();
+    }
+
+    void _on_cam_bootstrap_rgb_data(const unsigned char * _data, unsigned int _width, unsigned int _height,
+                                      unsigned int, const std::string &){
+        camera_impl->DisconnectNewImageFrame(sub_cam_rgb);
+
+        img_rgb = cv::Mat(_height, _width, CV_8UC3, (float_t*) _data);
+
+        cam_rgbI.onCameraSensorBoostrap(img_rgb, nullptr);
+        try{
+            Ice::Current c;
+            jderobot::ImageFormats formats = cam_rgbI.getImageFormat(c);
+            std::cout<<"getImageFormat :"<<formats[0]<<std::endl;
+        }catch(Ice::Exception &ex){
+            std::cout<<"getImageFormat :"<<ex<<std::endl;
+        }
+
+        sub_cam_rgb = camera_impl->ConnectNewImageFrame(
+                    boost::bind(&KinectPlugin::_on_cam_update_rgb_data, this, _1,_2,_3,_4,_5));
+    }
+
+    void _on_cam_update_rgb_data(const unsigned char *, unsigned int, unsigned int,
+                                   unsigned int, const std::string &){
+        cam_rgbI.onCameraSensorUpdate(img_rgb);
+    }
+
+#endif
+
+private:
+    gazebo::sensors::DepthCameraSensorPtr camera_sensor;
+    gazebo::rendering::DepthCameraPtr camera_impl;
+
+#ifdef PARENT_SENSOR_GETS_UPDATES
+    gazebo::event::ConnectionPtr sub_camera;
+#else
+    gazebo::event::ConnectionPtr sub_cam_depth;
+    gazebo::event::ConnectionPtr sub_cam_rgb;
+    gazebo::event::ConnectionPtr sub_cam_pcd;
+#endif
+
+    cv::Mat img_depth;
+    cv::Mat img_depth_raw;
+    cv::Mat img_rgb;
 
 /// Ice
     void InitializeIce(sdf::ElementPtr _sdf){
@@ -103,13 +285,23 @@ protected:
         Ice::ObjectAdapterPtr adapter;
         try{
         adapter = ic->createObjectAdapter("Kinect");
+
+        // pose
         adapter->add(this, ic->stringToIdentity("Pose3d"));
+
+        // cameras
+        adapter->add(&cam_depthI, ic->stringToIdentity("cameraDepth"));
+        adapter->add(&cam_rgbI, ic->stringToIdentity("cameraRGB"));
+
         adapter->activate();
         }catch(Ice::Exception &ex){ std::cout<< ex << std::endl;}
 
         std::cout<<_log_prefix << "Ice adapter listening at " << std::endl;
         std::cout<<_log_prefix << "\t" << adapter->getEndpoints()[0]->toString() << std::endl;
     }
+
+protected:
+    Ice::CommunicatorPtr ic;
 
 
 /// Ice (Pose3D)
@@ -133,17 +325,13 @@ public:
         return 0;
     }
 
+
+
+
+/// Ice (Camera)
 private:
-    gazebo::physics::ModelPtr model;
-    gazebo::event::ConnectionPtr updateConnection;
-    gazebo::event::ConnectionPtr sigintConnection;
-
-
-/// Ice
-protected:
-    Ice::CommunicatorPtr ic;
-
-private:
+    quadrotor::interfaces::PushCameraI cam_depthI;
+    quadrotor::interfaces::PushCameraI cam_rgbI;
 
 };
 
