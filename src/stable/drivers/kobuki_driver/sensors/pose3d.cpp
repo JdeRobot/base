ea6d63b1bda3fc94433c3e9ed352caf094e77aa6
Francisco Perez <f.perez475@gmail.com>
2016-06-20 10:54:07 +0200

    [issue #491] Added missing changes

--- src/stable/drivers/kobuki_driver/sensors/pose3d.cpp
+++ src/stable/drivers/kobuki_driver/sensors/pose3d.cpp
@@ -19,7 +19,10 @@ jderobot::Pose3DDataPtr Pose3D::getPose3DData(const Ice::Current&)
     jderobot::Pose3DDataPtr pose3dData(new jderobot::Pose3DData());
     pose3dData->x = kobuki->getRobotX();
     pose3dData->y = kobuki->getRobotY();
-    pose3dData->robottheta = 0;
+    pose3dData->q0 = 0.0;
+    pose3dData->q1 = 0.0;
+    pose3dData->q2 = 0.0;
+    pose3dData->q3 = 0.0;
     mutex.unlock();
     return pose3dData;
 }
