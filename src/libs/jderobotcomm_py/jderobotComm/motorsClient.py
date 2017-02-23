542b03429d06975364859bb227b18b0b3ac7fc2b
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2017-02-23 17:11:35 +0100

    [issue #679] Prepared JderobotComm for python 2 and 3

--- src/libs/jderobotcomm_py/jderobotComm/motorsClient.py
+++ src/libs/jderobotcomm_py/jderobotComm/motorsClient.py
@@ -1,10 +1,13 @@
+import sys
 import Ice
 import rospy
 from .ice.motorsIceClient import MotorsIceClient
-from .ros.publisherMotors import PublisherMotors
+
+if (sys.version_info[0] == 2):
+    from .ros.publisherMotors import PublisherMotors
 
 def __getMotorsIceClient(ic, prefix):
-	'''
+    '''
     Returns a Motors Ice Client. This function should never be used. Use getMotorsClient instead of this
 
     @param ic: Ice Communicator
@@ -16,13 +19,13 @@ def __getMotorsIceClient(ic, prefix):
     @return Motors Ice Client
 
     '''
-	print("Publishing Motors with ICE interfaces")
-	client = MotorsIceClient(ic, prefix)
-	client.start()
-	return client
+    print("Publishing Motors with ICE interfaces")
+    client = MotorsIceClient(ic, prefix)
+    client.start()
+    return client
 
 def __getPublisherMotors(ic, prefix):
-	'''
+    '''
     Returns a Motors ROS Publisher. This function should never be used. Use getMotorsClient instead of this
 
     @param ic: Ice Communicator
@@ -34,30 +37,34 @@ def __getPublisherMotors(ic, prefix):
     @return Motors ROS Publisher
 
     '''
-	print("Publishing Motors with ROS messages")
-	prop = prop = ic.getProperties()
-	topic = prop.getPropertyWithDefault(prefix+".Topic","");
-	client = PublisherMotors(topic)
-	return client
+    if (sys.version_info[0] == 2):
+        print("Publishing Motors with ROS messages")
+        prop = prop = ic.getProperties()
+        topic = prop.getPropertyWithDefault(prefix+".Topic","");
+        client = PublisherMotors(topic)
+        return client
+    else:
+        print(prefix + ": ROS msg are diabled for python "+ sys.version_info[0])
+        return None
 
 def __Motorsdisabled(ic, prefix):
-	'''
+    '''
     Prints a warning that the client is disabled. This function should never be used. Use getMotorsClient instead of this
 
     @param ic: Ice Communicator
     @param prefix: prefix name of client in config file
 
-	@type ic: Ice Communicator
+    @type ic: Ice Communicator
     @type prefix: String
 
     @return None
 
     '''
-	print(prefix + " Disabled")
-	return None
+    print(prefix + " Disabled")
+    return None
 
 def getMotorsClient (ic, prefix, node=None):
-	'''
+    '''
     Returns a Motors Client.
 
     @param ic: Ice Communicator
@@ -71,9 +78,9 @@ def getMotorsClient (ic, prefix, node=None):
     @return None if Motors is disabled
 
     '''
-	prop = prop = ic.getProperties()
-	server = prop.getPropertyAsIntWithDefault(prefix+".Server",0)
+    prop = prop = ic.getProperties()
+    server = prop.getPropertyAsIntWithDefault(prefix+".Server",0)
 
-	cons = [__Motorsdisabled, __getMotorsIceClient, __getPublisherMotors]
+    cons = [__Motorsdisabled, __getMotorsIceClient, __getPublisherMotors]
 
-	return cons[server](ic, prefix)
+    return cons[server](ic, prefix)
--- src/libs/jderobotcomm_py/jderobotComm/pose3dClient.py
+++ src/libs/jderobotcomm_py/jderobotComm/pose3dClient.py
@@ -1,10 +1,15 @@
+import sys
 import Ice
 import rospy
 from .ice.pose3dIceClient import Pose3dIceClient
-from .ros.listenerPose3d import ListenerPose3d
+
+if (sys.version_info[0] == 2):
+    from .ros.listenerPose3d import ListenerPose3d
+
+
 
 def __getPoseIceClient(ic, prefix):
-	'''
+    '''
     Returns a Pose3D Ice Client. This function should never be used. Use getPose3dClient instead of this
 
     @param ic: Ice Communicator
@@ -16,13 +21,13 @@ def __getPoseIceClient(ic, prefix):
     @return Pose3D Ice Client
 
     '''
-	print("Receiving Pose3D from ICE interfaces")
-	client = Pose3dIceClient(ic, prefix)
-	client.start()
-	return client
+    print("Receiving " + prefix + " from ICE interfaces")
+    client = Pose3dIceClient(ic, prefix)
+    client.start()
+    return client
 
 def __getListenerPose(ic, prefix):
-	'''
+    '''
     Returns a Pose3D ROS Subscriber. This function should never be used. Use getPose3dClient instead of this
 
     @param ic: Ice Communicator
@@ -34,30 +39,35 @@ def __getListenerPose(ic, prefix):
     @return Pose3D ROS Subscriber
 
     '''
-	print("Receiving Pose3D from ROS messages")
-	prop = prop = ic.getProperties()
-	topic = prop.getPropertyWithDefault(prefix+".Topic","");
-	client = ListenerPose3d(topic)
-	return client
+    if (sys.version_info[0] == 2):
+        print("Receiving " + prefix + " from ROS messages")
+        prop = prop = ic.getProperties()
+        topic = prop.getPropertyWithDefault(prefix+".Topic","");
+        client = ListenerPose3d(topic)
+        return client
+    else:
+        print(prefix + ": ROS msg are diabled for python "+ sys.version_info[0])
+        return None
+
 
 def __Posedisabled(ic, prefix):
-	'''
+    '''
     Prints a warning that the client is disabled. This function should never be used. Use getPose3dClient instead of this
 
     @param ic: Ice Communicator
     @param prefix: prefix name of client in config file
 
-	@type ic: Ice Communicator
+    @type ic: Ice Communicator
     @type prefix: String
 
     @return None
 
     '''
-	print(prefix + " Disabled")
-	return None
+    print(prefix + " Disabled")
+    return None
 
 def getPose3dClient (ic, prefix, node=None):
-	'''
+    '''
     Returns a Pose3D Client.
 
     @param ic: Ice Communicator
@@ -71,9 +81,9 @@ def getPose3dClient (ic, prefix, node=None):
     @return None if pose3d is disabled
 
     '''
-	prop = prop = ic.getProperties()
-	server = prop.getPropertyAsIntWithDefault(prefix+".Server",0)
+    prop = prop = ic.getProperties()
+    server = prop.getPropertyAsIntWithDefault(prefix+".Server",0)
 
-	cons = [__Posedisabled, __getPoseIceClient, __getListenerPose]
+    cons = [__Posedisabled, __getPoseIceClient, __getListenerPose]
 
-	return cons[server](ic, prefix)
+    return cons[server](ic, prefix)
