7e9d1806c48ebca7409612196d92a40d7c2398e3
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2017-07-28 10:00:14 +0200

    [issue #890] Solved installation bug in visualStates

--- src/tools/visualStates/setup.sh
+++ src/tools/visualStates/setup.sh
@@ -3,22 +3,22 @@
 # Copyright (c) 2016
 # Author: Samuel Rey <samuel.rey.escudero@gmail.com>
 # License: GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
-
+INSTALL_PREFIX=/opt/jderobot
 #C++ Dependencies
-sudo mkdir /usr/local/include/jderobot/visualStates
-sudo cp *.h  /usr/local/include/jderobot/visualStates
-sudo mkdir /usr/local/include/jderobot/visualStates/popups
-sudo cp popups/*.h /usr/local/include/jderobot/visualStates/popups/
-sudo mkdir /usr/local/share/jderobot/glade/visualStates
-sudo cp gui/* /usr/local/share/jderobot/glade/visualStates/
-sudo cp getinterfaces.sh /usr/local/bin
+sudo mkdir $INSTALL_PREFIX/include/jderobot/visualStates
+sudo cp *.h  $INSTALL_PREFIX/include/jderobot/visualStates
+sudo mkdir $INSTALL_PREFIX/include/jderobot/visualStates/popups
+sudo cp popups/*.h $INSTALL_PREFIX/include/jderobot/visualStates/popups/
+sudo mkdir $INSTALL_PREFIX/share/jderobot/glade/visualStates
+sudo cp gui/* $INSTALL_PREFIX/share/jderobot/glade/visualStates/
+sudo cp getinterfaces.sh $INSTALL_PREFIX/bin
 
 #Python Dependencies
-sudo mkdir /usr/local/share/jderobot/python/visualStates_py
-sudo cp *.py /usr/local/share/jderobot/python/visualStates_py
-sudo mkdir /usr/local/share/jderobot/python/visualStates_py/gui
-sudo cp gui/*.py /usr/local/share/jderobot/python/visualStates_py/gui
+sudo mkdir $INSTALL_PREFIX/share/jderobot/python/visualStates_py
+sudo cp *.py $INSTALL_PREFIX/share/jderobot/python/visualStates_py
+sudo mkdir $INSTALL_PREFIX/share/jderobot/python/visualStates_py/gui
+sudo cp gui/*.py $INSTALL_PREFIX/share/jderobot/python/visualStates_py/gui
 
-sudo cp visualStates /usr/local/bin
+sudo cp visualStates $INSTALL_PREFIX/bin
 
 echo "Dependencies installed"
