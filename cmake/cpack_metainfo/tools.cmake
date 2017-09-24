8f70d74cb65437e69fd10452dcfe1d11a0b4c462
Francisco <f.perez475@gmail.com>
2017-09-24 07:56:23 -0700

    Added some necessary ignored by git files

---
+++ cmake/cpack_metainfo/tools.cmake
@@ -0,0 +1,107 @@
+SET(CPACK_DEBIAN_3DVIEWER_PACKAGE_DEPENDS "jderobot-easyice, jderobot-viewer, jderobot-util, jderobot-interfaces, jderobot-logger, jderobot-ns, jderobot-colorspaces")
+SET(CPACK_COMPONENT_3DVIEWER_DESCRIPTION  
+"Generic openGL based 3D viewer for cameras. 
+ Manual Page http://jderobot.org/index.php/Tools#3DViewer
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_CAMERACALIBRATOR_PACKAGE_DEPENDS "jderobot-easyice,  jderobot-interfaces, jderobot-util, jderobot-parallelice, jderobot-colorspaces")
+SET(CPACK_COMPONENT_CAMERACALIBRATOR_DESCRIPTION  
+"Calibrator for extrinsics and intrinsics parameters of RGB cameras. 
+ Manual page http://jderobot.org/index.php/Tools#CameraCalibrator
+ Home page https://jderobot.org")
+
+ SET(CPACK_DEBIAN_COLORTUNER-PYTHON_PACKAGE_DEPENDS "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+SET(CPACK_COMPONENT_COLORTUNER-PYTHON_DESCRIPTION  
+"Tool for color filters and pixel treatment from multiple visual sources.
+ Manual page http://jderobot.org/index.php/Tools#ColorTuner_.28Python.29
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_CAMERAVIEW_PACKAGE_DEPENDS "jderobot-easyice, jderobot-comm, jderobot-util, jderobot-interfaces, jderobot-resourcelocator, jderobot-colorspaces")
+SET(CPACK_COMPONENT_CAMERAVIEW_DESCRIPTION  
+"Generic viewer for cameras 
+ Manual Page http://jderobot.org/index.php/Tools#CameraView
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_CAMERAVIEW-PYTHON_PACKAGE_DEPENDS "jderobot-easyice, jderobot-comm, jderobot-util, jderobot-interfaces, jderobot-resourcelocator, jderobot-colorspaces")
+SET(CPACK_COMPONENT_CAMERAVIEW-PYTHON_DESCRIPTION  
+"Generic viewer for cameras in python
+ Manual Page http://jderobot.org/index.php/Tools#CameraView
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_GIRAFFECLIENT_PACKAGE_DEPENDS "jderobot-easyice, jderobot-progeo, jderobot-util, jderobot-interfaces, jderobot-colorspaces")
+SET(CPACK_COMPONENT_GIRAFFECLIENT_DESCRIPTION  
+"Teleoperator for robotic necks and arms [deprecated] 
+ Manual Page 
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_KOBUKIVIEWER_PACKAGE_DEPENDS "jderobot-easyice, jderobot-comm")
+SET(CPACK_COMPONENT_KOBUKIVIEWER_DESCRIPTION  
+"Teleoperator for vehicle-type robots, such as kobuki, pioneer, cars, etc. 
+ Manual page http://jderobot.org/index.php/Tools#KobukiViewer
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_NAMINGSERVICE_PACKAGE_DEPENDS "jderobot-easyice")
+SET(CPACK_COMPONENT_NAMINGSERVICE_DESCRIPTION  
+" 
+ Manual Page 
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_PANTILTTELEOP_PACKAGE_DEPENDS "jderobot-easyice, jderobot-parallelice, jderobot-util, jderobot-interfaces, jderobot-resourcelocator, jderobot-colorspaces")
+SET(CPACK_COMPONENT_PANTILTTELEOP_DESCRIPTION  
+"Teleoperator for IP cameras that allow movement (i.e. Sony EVI camera)
+ Manual Page http://jderobot.org/index.php/Tools#NavigatorCamera
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_NAVIGATORCAMERA_PACKAGE_DEPENDS "jderobot-easyice, jderobot-parallelice, jderobot-util, jderobot-interfaces, jderobot-resourcelocator, jderobot-colorspaces")
+SET(CPACK_COMPONENT_NAVIGATORCAMERA_DESCRIPTION  
+"Teleoperator for simulated flying cameras.
+ Manual Page http://jderobot.org/index.php/Tools#NavigatorCamera
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_RECORDER2_PACKAGE_DEPENDS "jderobot-easyice, jderobot-util, jderobot-interfaces, jderobot-ns, jderobot-colorspaces")
+SET(CPACK_COMPONENT_RECORDER2_DESCRIPTION  
+"Tool for record logs of robots sensors and actuators.
+ Manual Page http://jderobot.org/index.php/Tools#Recorder
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_REPLAYCONTROLLER_PACKAGE_DEPENDS "jderobot-easyice, jderobot-viewer, jderobot-util, jderobot-interfaces, jderobot-logger, jderobot-ns, jderobot-colorspaces")
+SET(CPACK_COMPONENT_REPLAYCONTROLLER_DESCRIPTION  
+"Tool for controlling the recorded logs reproduction (play, pause, stop, ...) 
+ Manual Page http://jderobot.org/index.php/Tools#Replayer
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_REPLAYER2_PACKAGE_DEPENDS "jderobot-easyice, jderobot-resourcelocator, jderobot-util, jderobot-interfaces, jderobot-logger, jderobot-ns, jderobot-colorspaces")
+SET(CPACK_COMPONENT_REPLAYER2_DESCRIPTION  
+"Tool for replaying recorded logs with recorder2. 
+ Manual Page http://jderobot.org/index.php/Tools#Replayer
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_RGBDCALIBRATOR_PACKAGE_DEPENDS "jderobot-easyice, jderobot-progeo, jderobot-geometry, jderobot-interfaces, jderobot-resourcelocator, jderobot-cvblob, jderobot-colorspaces")
+SET(CPACK_COMPONENT_RGBDCALIBRATOR_DESCRIPTION  
+"Calibrator for RGB cameras.
+ Manual Page 
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_RGBDMANUALCALIBRATOR_PACKAGE_DEPENDS "jderobot-easyice, jderobot-progeo, jderobot-util, jderobot-interfaces, jderobot-resourcelocator, jderobot-parallelice, jderobot-colorspaces")
+SET(CPACK_COMPONENT_RGBDMANUALCALIBRATOR_DESCRIPTION  
+"Manual calibrator for RGBD cameras.
+ Manual Page 
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_RGBDVIEWER_PACKAGE_DEPENDS "jderobot-easyice, jderobot-geometry, jderobot-util, jderobot-interfaces, jderobot-parallelice, jderobot-pioneer, jderobot-progeo, jderobot-resourcelocator, jderobot-colorspaces")
+SET(CPACK_COMPONENT_RGBDVIEWER_DESCRIPTION  
+"Generic viewer for RGBD cameras. Included an openGL viewer for pointcloud representation. 
+ Manual Page http://jderobot.org/index.php/Tools#RGBDViewer
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_UAV-VIEWER_PACKAGE_DEPENDS "jderobot-easyice, jderobot-util, jderobot-interfaces, jderobot-colorspaces")
+SET(CPACK_COMPONENT_UAV-VIEWER_DESCRIPTION  
+"Teleoperator for UAV drones real and simulated (Parrot ArDrone, 3DR SoloDrone, ...) 
+ Manual Page http://jderobot.org/index.php/Tools#UAV_Viewer
+ Home page https://jderobot.org")
+
+SET(CPACK_DEBIAN_UAV-VIEWER-PYTHON_PACKAGE_DEPENDS "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+SET(CPACK_COMPONENT_UAV-VIEWER-PYTHON_DESCRIPTION  
+"Teleoperator for UAV drones real and simulated (Parrot ArDrone, 3DR SoloDrone, ...) for python
+ Manual Page http://jderobot.org/index.php/Tools#UAV_Viewer
+ Home page https://jderobot.org")
