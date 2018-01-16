7f9e111c4198491980870e3a13619e62ee8c50c2
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2018-01-16 13:11:16 +0100

    [#1023] added new tests to jenkins dockers

---
+++ scripts/docker/jenkins/dev/search_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+FILE_NOT_FONUD=64
+
+for f in $1/*; do
+	echo "$f"
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+	    if [ ! -f "$line" ]; then
+	    	echo "$line : File Not Found!" 1>&2
+			exit $FILE_NOT_FONUD
+		fi
+		#echo "$line"
+	done < "$f"
+done
