768060e9bb9befbd8452d985204e58e365569bf4
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2018-01-30 10:15:29 +0100

    New tests for packages

--- test/packages/3dviewer/search_file.sh
+++ test/packages/3dviewer/search_file.sh
@@ -4,7 +4,7 @@
 FILE_NOT_FONUD=64
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    if [ ! -f "$line" ]; then
+    if [ ! -f "$line" ] && [ ! -d "$line" ]; then
     	echo "File Not Found!" 1>&2
 		exit $FILE_NOT_FONUD
 	fi
