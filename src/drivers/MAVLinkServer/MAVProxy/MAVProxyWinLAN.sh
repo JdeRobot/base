bc86a305f5ec3eecef4772d743475131e1d51106
Diego Jimenez Bravo <diegojnb@gmail.com>
2018-02-03 19:15:38 +0100

    [#954] Ported to python 2.7 and integrated with comm

--- src/drivers/MAVLinkServer/MAVProxy/MAVProxyWinLAN.sh
+++ src/drivers/MAVLinkServer/MAVProxy/MAVProxyWinLAN.sh
@@ -1,5 +1,4 @@
 cd ..
 
 python2.7 setup.py build install --user
-python2.7 ./MAVProxy/mavproxy.py /home/diego/try3/2016-tfg-Diego-Jimenez/MAVProxy/uav_viewer_py.yml --master=10.1.1.191:14550 --console
-#python2.7 ./MAVProxy/mavproxy.py /home/diego/try3/2016-tfg-Diego-Jimenez/MAVProxy/uav_viewer_py.yml --master=0.0.0.0:14550 --console
+python2.7 ./MAVProxy/mavproxy.py ./MAVProxy/uav_viewer_py.yml --master=10.1.1.191:14550 --console
