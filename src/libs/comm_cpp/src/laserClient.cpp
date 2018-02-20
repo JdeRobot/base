baa4bd6e0b37c958507c708189caf805b0f2537f
Pushkal Katara <katarapushkal@gmail.com>
2018-02-20 16:08:33 +0530

    [Libs] Makes YML config file description more readable
    Instead of
    ```
    Server: 1
     or
    Server: 2
    ```
    Changed to
    ```
    Server: "Ice"
     or
    Server: "ROS"
    ```
    
    Solves issue https://github.com/JdeRobot/JdeRobot/issues/1060

--- src/libs/comm_cpp/src/laserClient.cpp
+++ src/libs/comm_cpp/src/laserClient.cpp
@@ -6,11 +6,21 @@
 
 namespace Comm {
 
-LaserClient* 
+LaserClient*
 getLaserClient(Comm::Communicator* jdrc, std::string prefix){
 	LaserClient* client = 0;
 
-	int server = jdrc->getConfig().asIntWithDefault(prefix+".Server", 0);
+	int server;
+	std::string server_name = jdrc->getConfig().asString(prefix+".Server");
+	std::transform(server_name.begin(), server_name.end(), server_name.begin(), ::tolower);
+	if(server_name == "ice"){
+		server = 1;
+	}
+	else if(server_name == "ros"){
+		server = 2;
+	}
+	else server = 0;
+
 	switch (server){
 		case 0:
 		{
