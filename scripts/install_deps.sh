d43bd1521558e484cedc2a60ffa0c112cda65430
Francisco Pérez <f.perez475@gmail.com>
2019-07-08 20:04:07 +0200

    Update dependencies installation script for 18.04

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-apt install sudo wget
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 67170598AF249743
 
-sudo apt-get update
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv B6391CB2CFBA643D
+sudo apt-add-repository "deb http://zeroc.com/download/Ice/3.7/ubuntu18.04 stable main"
 
-sudo apt install -y ros-kinetic-roscpp ros-kinetic-std-msgs ros-kinetic-cv-bridge ros-kinetic-image-transport ros-kinetic-roscpp-core ros-kinetic-rospy ros-kinetic-nav-msgs ros-kinetic-geometry-msgs ros-kinetic-opencv3 ros-kinetic-kobuki-gazebo
+sudo sh -c 'cat<<EOF>/etc/apt/sources.list.d/jderobot.list
+# for ubuntu 16.04 LTS (64 bit)
 
-sudo apt-get install -y libpcl-dev
-
-sudo apt-add-repository "deb http://zeroc.com/download/apt/ubuntu$(lsb_release -rs) stable main"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 5E6DA83306132997
-
-
-sudo sh -c 'echo "deb http://jderobot.org/apt xenial main" > /etc/apt/sources.list.d/jderobot.list'
-sudo apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys B0E7F58E82C8091DF945A0453DA08892EE69A25C
+deb [arch=amd64] http://jderobot.org/apt bionic main
+EOF'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 24E521A4
 
 sudo apt update
 
+sudo apt install wget
+
+sudo apt install -y ros-melodic-roscpp ros-melodic-std-msgs ros-melodic-cv-bridge ros-melodic-image-transport ros-melodic-roscpp-core ros-melodic-rospy ros-melodic-nav-msgs ros-melodic-geometry-msgs ros-melodic-kobuki-msgs
+sudo apt-get install -y libpcl-dev
+sudo apt-get install libopencv-dev
 sudo apt install -y build-essential libtool cmake g++ gcc git make
 
 sudo apt install -y freeglut3 freeglut3-dev libgl1-mesa-dev libglu1-mesa
@@ -31,7 +34,7 @@ sudo apt-get install -y libgnomecanvasmm-2.6-1v5 libgtkglextmm-x11-1.2-0v5 libgt
 
 sudo apt-get install -y libgoocanvasmm-2.0-6 libgoocanvasmm-2.0-dev
 
-sudo apt-get install -y libgsl2 gsl-bin libgsl-dev
+sudo apt-get install -y libgsl23 gsl-bin libgsl-dev
 
 sudo apt-get install -y libxml++2.6-2v5 libxml++2.6-dev libtinyxml-dev
 
@@ -45,13 +48,13 @@ sudo apt-get install -y python-matplotlib python-pyqt5 python-pip python-numpy p
 
 sudo apt-get install -y qfi
 
-sudo apt-get install -y qtbase5-dev libqt5declarative5 libqt5script5 libqt5svg5-dev
+sudo apt-get install -y qtbase5-dev libqt5script5 libqt5svg5-dev
 
 sudo apt-get install -y libboost-system-dev libboost-filesystem-dev
 
-sudo apt-get install -y libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 
-sudo apt install -y libzeroc-ice3.6 zeroc-ice-utils libzeroc-icestorm3.6 zeroc-ice-slice libzeroc-ice-dev
+sudo apt install -y libzeroc-ice3.7 libzeroc-icestorm3.7 zeroc-ice-slice libzeroc-ice-dev
 
 sudo apt-get install -y libssl-dev libbz2-dev
 sudo pip2 install --upgrade pip
@@ -59,13 +62,13 @@ sudo pip2 install zeroc-ice==3.6.4
 
 sudo apt-get install -y libopenni2-dev libopenni-dev
 
-sudo apt-get install -y gazebo7 libgazebo7-dev
+sudo apt-get install -y gazebo9 libgazebo9-dev
 
 sudo apt-get install -y nodejs
 
 sudo apt-get install -y ardronelib libgoogle-glog-dev
 
-
+sudo apt install libyaml-cpp-dev
 
 
 
